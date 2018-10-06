02e5822966c9450d01a2e9e75a281f9f878cd507
jxv <joevargas92@gmail.com>
2018-10-06 13:54:25 -0700

    data structs

--- library/Sugar.hs
+++ library/Sugar.hs
@@ -1,2 +1,43 @@
 module Sugar where
 
+import Data.Text (Text)
+import Data.Map (Map)
+
+type Name = Text
+type Var = Text
+type Field = Text
+
+data Type = Type
+    { tName :: Name
+    , tParam :: [Var]
+    } deriving (Show, Eq)
+
+data Alias = Alias
+    { aName :: Type
+    , aDefinition :: Type
+    } deriving (Show, Eq)
+
+data Record = Record
+    { rType :: Type
+    , rFields :: Map Field Type
+    } deriving (Show, Eq)
+
+data Variant = Variant
+    { vType :: Type
+    , vCases :: [Case]
+    } deriving (Show, Eq)
+
+data Case
+    = Case'Ref Type
+    | Case'Record Record
+    | Case'Variant Variant
+    | Case'Tag Name
+    deriving (Show, Eq)
+
+data Decl
+    = Decl'Alias Alias
+    | Decl'Record Record
+    | Decl'Variant Variant
+    deriving (Show, Eq)
+
+type File = [Decl]
