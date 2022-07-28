96e1f993fbc04591901ed5b45fd5996a0278f395
jxv <3070597+jxv@users.noreply.github.com>
2022-07-28 04:40:35 -0700

    clean up tests

--- sugar/tests/Main.hs
+++ sugar/tests/Main.hs
@@ -1,11 +1,10 @@
 {-# LANGUAGE TupleSections, DeriveGeneric, OverloadedStrings, CPP #-}
-import Sugar
+
 import qualified Test.Tasty
 import Test.Tasty.Hspec
 import Test.Hspec
 
-import Sugar.Lexer
-import Sugar.Parser
+import Sugar
 
 main :: IO ()
 main = do
@@ -16,54 +15,21 @@ spec :: Spec
 spec = parallel $ do
   it "unit" $ do
     let actual = map snd $ psSteps $ sugarLexerState "()"
-    let expected = [Token'OpenParen, Token'CloseParen]
+    let expected = [Lexeme'OpenParen, Lexeme'CloseParen]
+    actual `shouldBe` expected
+  it "string" $ do
+    let actual = map snd $ psSteps $ sugarLexerState "hello"
+    let expected = [Lexeme'StringStart, Lexeme'String "hello"]
     actual `shouldBe` expected
   it "empty map" $ do
     let actual = map snd $ psSteps $ sugarLexerState "{}"
-    let expected = [Token'OpenCurl, Token'CloseCurl]
+    let expected = [Lexeme'OpenCurl, Lexeme'CloseCurl]
     actual `shouldBe` expected
-  --
-  it "" $ do
-    True `shouldBe` True
-
-mySugar :: Sugar
-mySugar = Sugar'Map [(Sugar'Text "key" Nothing, Sugar'Text "value" Nothing)] Nothing
-
-mySugar' :: Sugar
-mySugar' = Sugar'Map [
-    (Sugar'Text "key" (Just "A-che-ora"), Sugar'Text "value" (Just "A che ora")),
-    (Sugar'Text "anotherKey" Nothing, Sugar'Map
-      [
-        (Sugar'Text "a" Nothing, Sugar'Text "12345\"6789" Nothing),
-        (Sugar'Text "c" Nothing, Sugar'Text "12345    6789" Nothing),
-        (Sugar'Text "e" Nothing, Sugar'Text "123'456789" Nothing),
-        (Sugar'Text "d" Nothing, Sugar'List ["", "", Sugar'List [Sugar'List [Sugar'List ["","",Sugar'Map [("",Sugar'Map [] Nothing),("",Sugar'Map [] Nothing)] Nothing] Wrap'Paren Nothing] Wrap'Square Nothing]Wrap'Paren Nothing] Wrap'Square Nothing)
-      ]
-      (Just "Hello"))
-  ] Nothing
-
-data Color
-  = Color'R
-  | Color'G
-  | Color'B
-  deriving (Show, Eq)
-
-instance FromSugar Color where
-  parseSugar (Sugar'Text a _) = case a of
-    "R" -> Just Color'R
-    "G" -> Just Color'G
-    "B" -> Just Color'B
-    _ -> Nothing
-  parseSugar _ = Nothing
-
-keyVal :: IO ()
-keyVal = readSugarFromFile "keyValue.sg" >>= \(Just sg) -> prettyPrintSugarIO sg
-
-large :: IO ()
-large = readSugarFromFile "large.sg" >>= \(Just sg) -> prettyPrintSugarIO sg
-
-mysugar :: IO ()
-mysugar = readSugarFromFile "mysugar.sg" >>= \(Just sg) -> prettyPrintSugarIO sg
-
-noline :: IO ()
-noline = readSugarFromFile "noline.sg" >>= \(Just sg) -> prettyPrintSugarIO sg
+  it "list" $ do
+    let actual = map snd $ psSteps $ sugarLexerState "[]"
+    let expected = [Lexeme'OpenSquare, Lexeme'CloseSquare]
+    actual `shouldBe` expected
+  it "angle" $ do
+    let actual = map snd $ psSteps $ sugarLexerState "<>"
+    let expected = [Lexeme'OpenAngle, Lexeme'CloseAngle]
+    actual `shouldBe` expected
