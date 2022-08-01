448991f2fa12c5fdf86182596c67317a36870475
jxv <3070597+jxv@users.noreply.github.com>
2022-08-01 11:07:20 -0700

    sugar-scheme: *ToLispVal can fail and parse tokens

--- sugar-scheme/src/Sugar/Scheme.hs
+++ sugar-scheme/src/Sugar/Scheme.hs
@@ -1,6 +1,7 @@
 {-# LANGUAGE OverloadedStrings #-}
 module Sugar.Scheme
-  ( toLispVal
+  ( tokenToLispVal
+  , sugarToLispVal
   ) where
   
 import Sugar
@@ -12,12 +13,11 @@ import qualified Text.Parsec.Token as P
 import Text.ParserCombinators.Parsec
 import Text.Parsec.Language ()
 
-
-toLispVal :: Sugar -> LispVal
-toLispVal (Sugar'Unit _) = List [] -- TODO: should this be 'nil'?
-toLispVal (Sugar'Text txt _) =  case parse valParser "valParser" (unpack txt) of
-  Left _ -> String (unpack txt)
-  Right v -> v
+tokenToLispVal :: TokenStep -> Either (SourceLocation, String) LispVal
+tokenToLispVal (_, Token'Unit _) = Right nullLisp
+tokenToLispVal (loc, Token'Text txt _) =  case parse valParser "valParser" txt of
+  Left _ -> Left (loc,txt)
+  Right v -> Right v
   where
     lexeme = P.lexeme lexer
     lexer = P.makeTokenParser lispDef
@@ -32,5 +32,27 @@ toLispVal (Sugar'Text txt _) =  case parse valParser "valParser" (unpack txt) of
       <|> parseQuoted
       <|> parseQuasiQuoted
       <|> parseUnquoted
-toLispVal (Sugar'List ls _ _) = List (map toLispVal ls)
-toLispVal (Sugar'Map m _) = HashTable $ Map.fromList $ map (\(x,y) -> (toLispVal x, toLispVal y)) m
+tokenToLispVal (_, Token'List ls _ _) = List <$> mapM tokenToLispVal ls
+tokenToLispVal (_, Token'Map m _) = (HashTable . Map.fromList) <$> mapM (\(x,y) -> (,) <$> tokenToLispVal x <*> tokenToLispVal y) m
+
+sugarToLispVal :: Sugar -> Either String LispVal
+sugarToLispVal (Sugar'Unit _) = Right nullLisp
+sugarToLispVal (Sugar'Text txt _) =  case parse valParser "valParser" (unpack txt) of
+  Left _ -> Left (unpack txt)
+  Right v -> Right v
+  where
+    lexeme = P.lexeme lexer
+    lexer = P.makeTokenParser lispDef
+    valParser =
+          try (lexeme parseComplexNumber)
+      <|> try (lexeme parseRationalNumber)
+      <|> try (lexeme parseRealNumber)
+      <|> try (lexeme parseNumber)
+      <|> try parseAtom
+      <|> lexeme parseString
+      <|> lexeme parseBool
+      <|> parseQuoted
+      <|> parseQuasiQuoted
+      <|> parseUnquoted
+sugarToLispVal (Sugar'List ls _ _) = List <$> mapM sugarToLispVal ls
+sugarToLispVal (Sugar'Map m _) = (HashTable . Map.fromList) <$> mapM (\(x,y) -> (,) <$> sugarToLispVal x <*> sugarToLispVal y) m
