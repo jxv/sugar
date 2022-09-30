19be2cd43bf44f47b0d13bd99aa042bf78dcc428
jxv <3070597+jxv@users.noreply.github.com>
2022-09-29 17:24:27 -0700

    sugar-scheme: update to properly parse strings

--- sugar-scheme/src/Sugar/Scheme.hs
+++ sugar-scheme/src/Sugar/Scheme.hs
@@ -15,47 +15,42 @@ import qualified Data.Map as Map
 import qualified Text.Parsec.Token as P
 import Text.ParserCombinators.Parsec
 import Text.Parsec.Language ()
+import Text.Parsec (Parsec)
+import Text.Parsec.Token (TokenParser)
 
 tokenToLispVal :: P.TokenStep -> Either (SourceLocation, String) LispVal
 tokenToLispVal (_, P.Unit _) = Right nullLisp
-tokenToLispVal (loc, P.Text txt _) =  case parse valParser "valParser" txt of
+tokenToLispVal (_, P.Text txt Sg.HasQuote _) = pure $ String txt
+tokenToLispVal (loc, P.Text txt Sg.NoQuote _) =  case parse valParser "valParser" txt of
   Left _ -> Left (loc,txt)
   Right v -> Right v
-  where
-    lexeme = P.lexeme lexer
-    lexer = P.makeTokenParser lispDef
-    valParser =
-          try (lexeme parseComplexNumber)
-      <|> try (lexeme parseRationalNumber)
-      <|> try (lexeme parseRealNumber)
-      <|> try (lexeme parseNumber)
-      <|> try parseAtom
-      <|> lexeme parseString
-      <|> lexeme parseBool
-      <|> parseQuoted
-      <|> parseQuasiQuoted
-      <|> parseUnquoted
 tokenToLispVal (_, P.List ls _ _) = List <$> mapM tokenToLispVal ls
 tokenToLispVal (_, P.Map m _) = (HashTable . Map.fromList) <$> mapM (\(x,y) -> (,) <$> tokenToLispVal x <*> tokenToLispVal y) m
 
 sugarToLispVal :: Sugar -> Either String LispVal
 sugarToLispVal (Sg.Unit _) = Right nullLisp
-sugarToLispVal (Sg.Text txt _) =  case parse valParser "valParser" (unpack txt) of
+sugarToLispVal (Sg.Text txt Sg.HasQuote _) = pure $ String (unpack txt)
+sugarToLispVal (Sg.Text txt Sg.NoQuote _) =  case parse valParser "valParser" (unpack txt) of
   Left _ -> Left (unpack txt)
   Right v -> Right v
-  where
-    lexeme = P.lexeme lexer
-    lexer = P.makeTokenParser lispDef
-    valParser =
-          try (lexeme parseComplexNumber)
-      <|> try (lexeme parseRationalNumber)
-      <|> try (lexeme parseRealNumber)
-      <|> try (lexeme parseNumber)
-      <|> try parseAtom
-      <|> lexeme parseString
-      <|> lexeme parseBool
-      <|> parseQuoted
-      <|> parseQuasiQuoted
-      <|> parseUnquoted
 sugarToLispVal (Sg.List ls _ _) = List <$> mapM sugarToLispVal ls
 sugarToLispVal (Sg.Map m _) = (HashTable . Map.fromList) <$> mapM (\(x,y) -> (,) <$> sugarToLispVal x <*> sugarToLispVal y) m
+
+lexeme :: Parsec String () a -> Parsec String () a
+lexeme = P.lexeme lexer
+
+lexer :: TokenParser ()
+lexer = P.makeTokenParser lispDef
+
+valParser :: Parsec String () LispVal
+valParser =
+  try (lexeme parseComplexNumber)
+  <|> try (lexeme parseRationalNumber)
+  <|> try (lexeme parseRealNumber)
+  <|> try (lexeme parseNumber)
+  <|> try parseAtom
+  <|> lexeme parseBool
+  <|> parseQuoted
+  <|> parseQuasiQuoted
+  <|> parseUnquoted
+
